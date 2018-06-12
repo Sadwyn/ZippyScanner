975665fd5194691a046c79d48a3c31ba17a9d41d
Sadwyn <sadwyn1@gmail.com>
2018-06-12 20:31:01 +0300

    code has been cleaned up

--- app/src/main/java/com/diploma/sadwyn/zippyscanner/google/vision/OcrGraphic.java
+++ app/src/main/java/com/diploma/sadwyn/zippyscanner/google/vision/OcrGraphic.java
@@ -1,43 +1,12 @@
-/*
- * Copyright (C) The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.diploma.sadwyn.zippyscanner.google.vision;
 
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
-import android.util.Log;
 
-import com.diploma.sadwyn.zippyscanner.App;
-import com.google.android.gms.vision.text.Element;
-import com.google.android.gms.vision.text.Line;
 import com.google.android.gms.vision.text.Text;
-import com.google.android.gms.vision.text.TextBlock;
-import com.google.cloud.translate.Translate;
-import com.google.cloud.translate.TranslateOptions;
-import com.google.cloud.translate.Translation;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import io.reactivex.Scheduler;
-import io.reactivex.Single;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 
 public class OcrGraphic extends GraphicOverlay.Graphic {
     private int mId;
